90eac5ed50100c55de5b049d93b6b19d2aaed569
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2024-06-10 11:31:59 +0330

    Update ⁨تعیین اول بودن عدد.py⁩

--- تعیین اول بودن عدد.py
+++ تعیین اول بودن عدد.py
@@ -1,11 +1,18 @@
-x = input()
-x=int(x)
-c=0
-for i in range(1,x):
-    if (x%i==0):
-        c+=1
-if (c==1):
-    print("prime")
+def is_prime(x):
+    c = 0
+    for i in range(1, x):
+        if x % i == 0:
+            c += 1
+    if c == 1:
+        return True
+    else:
+        return False
 
+# دریافت ورودی از کاربر
+x = int(input("Enter a number: "))
+
+# بررسی عدد و چاپ نتیجه
+if is_prime(x):
+    print("prime")
 else:
     print("not prime")
